d24738408c1a92c24a65c2ebb4f99bb72db2b8e3
TuanVu <anhtuandev2001@gmail.com>
2025-04-03 18:06:10 +0700

    add

--- client/src/utils/constants/strapi.ts
+++ client/src/utils/constants/strapi.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-expect-error
 const BASE_URL = import.meta.env.VITE_API_URL;
+// @ts-expect-error
 const Province_URL = import.meta.env.VITE_PROVINCE_URL;
+// @ts-expect-error
 const MAILER_URL = import.meta.env.VITE_MAILER_URL;
 
 export { BASE_URL, Province_URL, MAILER_URL };
