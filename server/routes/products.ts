470c68c40ca808583522cc995192c0964fac5d29
TuanVu <anhtuandev2001@gmail.com>
2025-05-09 15:48:42 +0700

    Enhance product routes by adding endpoints for retrieving products by ID and searching by keyword

--- server/routes/products.ts
+++ server/routes/products.ts
@@ -5,13 +5,14 @@ const router = express.Router();
 
 router.get('/', productController.getProducts);
 router.get('/getCount', productController.getCount);
-router.patch('/', productController.updateProduct);
-router.post('/', productController.insertProduct);
-router.delete('/:productId', productController.deleteProduct);
-router.get('/:productId', productController.getProductById);
-router.post('/getCarts', productController.getCarts);
-router.get('/search/:keyword', productController.searchProduct);
 router.get('/discount', productController.getProductsWithDiscount);
 router.get('/latest', productController.getLatestProducts);
+router.get('/:productId', productController.getProductById);
+router.get('/search/:keyword', productController.searchProduct);
+
+router.patch('/', productController.updateProduct);
+router.post('/', productController.insertProduct);
+router.post('/getCarts', productController.getCarts);
+router.delete('/:productId', productController.deleteProduct);
 
 export default router;
